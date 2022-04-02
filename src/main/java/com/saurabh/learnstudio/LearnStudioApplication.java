7e896d6494708291133f5bec708717fc05392023
Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>
2022-04-02 22:15:31 +0530

    Initial Commit

---
+++ src/main/java/com/saurabh/learnstudio/LearnStudioApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.learnstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saurabh/learnstudio/LearnStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.learnstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
